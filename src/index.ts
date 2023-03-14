1839f0ea4e5c5f8593f6d91049a738db98eaa916
guilhermebauer-builders <guilherme.bauer@platformbuilders.io>
2023-03-14 11:16:57 -0300

    feature(newOrgs): new options

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,72 @@
 #! /usr/bin/env node
-import readline from 'readline'
+// import readline from 'readline'
 import os from 'os'
 import fs from 'fs'
+import inquirer from 'inquirer'
 
-const askQuestion = (query: string) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+const startInquirer = async () => {
+  const promptToken: Record<'token', string> = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'token',
+      message: '- Entre nesse site: https://github.com/settings/tokens\n- Crie um token com o escopo de, no mínimo, \'REPO\' e \'READ:PACKAGES\'\n- Cole o token gerado aqui:'
+    }
+  ])
 
-  const readAnswer = (ans: string) => {
-    const platform = process.platform
-    const path = platform === 'linux' ? '/home' : '/Users'
-    const data = `@platformbuilders:registry=https://npm.pkg.github.com/\n//npm.pkg.github.com/:_authToken=${ans}\n`
+  const promptOrganizations: Record<'organizations', string[]> = await inquirer.prompt(
+    [
+      {
+        type: 'checkbox',
+        message: 'Deseja ter acesso aos packages de quais organizações?',
+        name: 'organizations',
+        choices: [
+          {
+            name: 'PlatformBuilders',
+            value: '@platformbuilders'
+          },
+          {
+            name: 'BuildersBank',
+            value: '@buildersbank'
+          },
+          {
+            name: 'Outra organização',
+            value: '@custom_org'
+          }
+        ],
+        validate (answer) {
+          if (answer.length < 1) {
+            return 'Você deve escolher pelo menos uma opção'
+          }
 
-    fs.appendFileSync(`${path}/${os.userInfo().username}/.npmrc`, data)
-    rl.close()
+          return true
+        }
+      }
+    ]
+  )
+
+  const haveCustomOrg = promptOrganizations.organizations.includes('@custom_org')
+
+  if (haveCustomOrg) {
+    const promptCustomOrg: Record<'customOrg', string> = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'customOrg',
+        message: 'Digite aqui o escopo da organização (ex: @platformbuilders):'
+      }
+    ])
+
+    promptOrganizations.organizations.splice(promptOrganizations.organizations.length - 1, 1, promptCustomOrg.customOrg)
+  } else {
+    promptOrganizations.organizations.splice(promptOrganizations.organizations.length - 1, 0)
   }
 
-  return rl.question(query, readAnswer)
+  const platform = process.platform
+  const path = platform === 'linux' ? '/home' : '/Users'
+
+  promptOrganizations.organizations.forEach(organization => {
+    const data = `${organization}:registry=https://npm.pkg.github.com/\n//npm.pkg.github.com/:_authToken=${promptToken.token}\n`
+    fs.appendFileSync(`${path}/${os.userInfo().username}/.npmrc`, data)
+  })
 }
 
-askQuestion(`
-  - Entre nesse site: https://github.com/settings/tokens
-  - Crie um token com o escopo de, no mínimo, 'REPO' e 'READ:PACKAGES'
-  - Cole o token gerado aqui: `)
+startInquirer()
